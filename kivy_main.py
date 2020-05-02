88d953aec906e94fe67a97b927955c7a248f8330
Илья Евсеенко <addminuser258@gmail.com>
2020-05-02 22:19:23 +0300

    эксперимент с созданием файла

--- kivy_main.py
+++ kivy_main.py
@@ -1,8 +1,19 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.network.urlrequest import UrlRequest
 
 class TestApp(App):
     def build(self):
-        return Button(text='Hello World')
+        button = Button(text='Hello from Kivy')
+                        
+        button.bind(on_press=self.on_press_button)
+ 
+        return button
+
+    def on_press_button(self, instance):
+        print('Вы нажали на кнопку!')
+        f = open("test.txt", "w")
+        f.write("hello")
+        
 
 TestApp().run()
